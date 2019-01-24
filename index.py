1c859c360099d69cd60db62d0b7df79bd84f5741
Pasang Dimdung <pasangdimdung045@gmail.com>
2019-01-24 20:32:20 +0545

    Dash using Dash

---
+++ index.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
+import numpy as np
+
+#RANDOM DATA GENERATION
+x_values = np.linspace(0,1,100)
+y_values = np.random.randn(100)
+
+app = dash.Dash()
+
+app.layout= html.Div([
+            dcc.Dropdown(
+
+                        id= 'my-dropdown',
+                        options=[
+                        {'label': 'By Purpose','value': 'BP'},
+                        {'label': 'By Gender','value': 'BG'},
+                        {'label': 'By Month','value': 'BM'},
+                        {'label': 'By number of Trekkers','value': 'BNT'}
+                        ],
+                        value = 'BM')
+                        ,
+
+            dcc.Graph(id='scatterplot',
+                    figure= { 'data'  : [go.Scatter(x=x_values, y=y_values+5, mode='lines+markers', name='LM')],
+                              'layout': go.Layout(title='Tourist arrival by month from 1992-2013',
+                                                  xaxis= {'title': 'Months'},
+                                                  yaxis= {'title': 'No of Tourist'})
+                            })
+                     ])
+                     
+if __name__ == '__main__':
+    app.run_server()
