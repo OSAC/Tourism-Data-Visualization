3c7df7ac615986fbe51752ce2c0668d33fd5eeb2
Vuown <bhuwanaryal19@gmail.com>
2019-01-28 16:37:43 +0545

    Index updated for Months

--- index.py
+++ index.py
@@ -5,11 +5,9 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
 import numpy as np
+import pandas as pd
 
-#RANDOM DATA GENERATION
-x_values = np.linspace(0,1,100)
-y_values = np.random.randn(100)
-
+by_month_df = pd.read_csv('data/by_month_year')
 app = dash.Dash()
 
 app.layout= html.Div([
@@ -26,7 +24,8 @@ app.layout= html.Div([
                         ,
 
             dcc.Graph(id='scatterplot',
-                    figure= { 'data'  : [go.Scatter(x=x_values, y=y_values+5, mode='lines+markers', name='LM')],
+                    figure= { 'data'  : [go.Scatter(x=by_month_df.Month,
+                     y= by_month_df.Visitors, mode='lines+markers', name='LM')],
                               'layout': go.Layout(title='Tourist arrival by month from 1992-2013',
                                                   xaxis= {'title': 'Months'},
                                                   yaxis= {'title': 'No of Tourist'})
