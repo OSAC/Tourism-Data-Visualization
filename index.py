22fb4d35b4920670c257e4cc6a7fe885f9065cc7
Pasang Dimdung <pasangdimdung045@gmail.com>
2019-01-29 12:01:07 +0545

    dropdown and slider connected

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
+#IMPORTS
 import dash
 import dash_core_components as dcc
-import dash_core_components 
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
@@ -8,48 +8,92 @@ import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
 
+# DATA
 by_month_df = pd.read_csv('data/by_month_year')
+by_year = pd.read_csv('data/modified_visitors_by_month', index_col = [0])
+
+
 app = dash.Dash()
 
+#MAIN APP
 app.layout = html.Div([
-    dash_core_components.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=by_month_df['Year'].min(),
-        max=by_month_df['Year'].max(),
-        value=by_month_df['Year'].min(),
-        marks={str(year): str(year) for year in by_month_df['Year'].unique()}
-    )
-])
 
-#app.config['suppress_callback_exceptions']=True                    
-@app.callback(
-    dash.dependencies.Output('graph-with-slider', 'figure'),
-    [dash.dependencies.Input('year-slider', 'value')])
+                html.H4(' Dash Board'),
+                html.P(),
+                 dcc.Dropdown(
+                    id= 'options-dropdown',
+                    options=[
+                        #{'label': 'By Purpose','value': 'BP'},
+                        #{'label': 'By Gender','value': 'BG'},
+                        {'label': 'By Month','value': 'BM'},
+                        {'label': 'By Year','value': 'BY'},
+                        #{'label': 'By number of Trekkers','value': 'BNT'}
+                            ],
+                    value = 'BM'
+                               ),
 
-def update_figure(selected_year):
-    filtered_df = by_month_df[by_month_df.Year == selected_year]
-    traces = []
-    
-    traces.append(go.Scatter(
-        x = filtered_df['Month'].values,
-        y = filtered_df['Visitors'].values,
-        mode='markers',
-        opacity=0.7,
-        marker={
-            'size': 15,
-            'line': {'width': 0.5, 'color': 'white'}
-        }
-        ))
+                 dcc.Graph(
+                    id='graph-with-slider'),
 
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis={ 'title': 'Months'},
-            yaxis={'title': 'No of visitors'},
-            hovermode='closest'
-        )
-    }
+                 html.Label('Data Range'),
+                 html.P(),
+
+                 dcc.Slider(
+                    id='year-slider',
+                    min=by_month_df['Year'].min(),
+                    max=by_month_df['Year'].max(),
+                    value=by_month_df['Year'].min(),
+                    marks={str(year): str(year) for year in by_month_df['Year'].unique()}
+                            )
+                     ])
+
+#DECORATOR
+@app.callback(Output('graph-with-slider', 'figure'),
+             [Input('year-slider', 'value'), Input('options-dropdown', 'value')])
+
+def update_figure(selected_year, selected_option):
+    if selected_option == 'BM':
+        filtered_df = by_month_df[by_month_df.Year == selected_year]
+
+        traces = []
+
+        traces.append( go.Scatter(
+                         x = filtered_df['Month'].values,
+                         y = filtered_df['Visitors'].values,
+                         mode='markers',
+                         opacity=0.7,
+                         marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                                }
+                                    )
+                      )
+
+        return {
+            'data': traces,
+            'layout': go.Layout(
+                        title='Tourists Arrival By Months',
+                        xaxis={ 'title': 'Months'},
+                        yaxis={'title': 'No of visitors'},
+                        hovermode='closest'
+                                )
+                }
+
+    elif selected_option == 'BY':
+            return {
+                'data' : [go.Scatter(
+                            x = by_year.date.values,
+                            y = by_year.Visitors.values,
+                            mode = 'lines+markers'
+                                    )
+                         ],
+                 'layout': go.Layout(
+                             title= 'Tourist Arrival By Year',
+                             xaxis={ 'title': 'Years (1992-2013)'},
+                             yaxis={'title': 'No of visitors'},
+                             hovermode='closest'
+                 )
+                    }
 
 if __name__ == '__main__':
     app.run_server()
